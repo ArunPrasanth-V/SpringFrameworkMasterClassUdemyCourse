5154985200dfd322cb717129ed56d99db149557b
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-22 13:38:19 +0530

    Create BinarySearch.java

---
+++ DependencyInjectionWithoutSpring/BinarySearch.java
@@ -0,0 +1,17 @@
+package com.ArunJavaProgrammer;
+
+public class BinarySearch {
+	SortRule sorting;
+	BinarySearch(SortRule sorting)
+	{
+		this.sorting=sorting;
+	}
+	public int searching(int[] arr)
+	{
+		sorting.sort(arr);
+		return 3;
+	}
+	
+	
+
+}
