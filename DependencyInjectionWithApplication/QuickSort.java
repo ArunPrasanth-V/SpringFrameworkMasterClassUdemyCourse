33bbc5d2004aa9ebff6c85b5f1642a290681e930
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-22 22:41:11 +0530

    Create QuickSort.java

---
+++ DependencyInjectionWithApplication/QuickSort.java
@@ -0,0 +1,15 @@
+package com.ArunJavaProgrammer;
+import org.springframework.stereotype.Component;
+
+@Component("quick")
+public class QuickSort implements SortRule {
+	public int[] sort(int[] arr)
+	{
+		System.out.println("Performing QuickSort");
+
+		//sort the number
+		
+		return arr;
+	}
+
+}
